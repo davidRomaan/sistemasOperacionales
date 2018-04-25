9ad3da77072c0ade729e31c3846d49bc24e274c3
davidRomaan <romanleon2010@hotmail.com>
2018-04-25 10:48:03 -0500

    commit 25/04/18

--- herramientaCRM/crm-negocio/src/main/java/co/edu/eam/ingesoft/bi/negocio/beans/UsuarioEJB.java
+++ herramientaCRM/crm-negocio/src/main/java/co/edu/eam/ingesoft/bi/negocio/beans/UsuarioEJB.java
@@ -1,5 +1,27 @@
 package co.edu.eam.ingesoft.bi.negocio.beans;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import co.edu.eam.ingesoft.bi.presistencia.entidades.Usuario;
+
+
+@Stateless
 public class UsuarioEJB {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	
+	public Usuario buscarUsuario(String user) {
+		List<Usuario>us= em.createNamedQuery("Usuario.buscarUsuario").setParameter(1, user).getResultList();
+		if(us.isEmpty()){
+			return null;
+		}else{
+			return us.get(0);
+		}
+	}
 
 }
