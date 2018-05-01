84f3b943d6e52cf401ae6c1d5ed7dd0f2efeb30f
LuchoBolivar <tennluis77@gmail.com>
2018-04-30 21:03:13 -0500

    holis

---
+++ herramientaCRM/crm-persistencia/src/main/java/co/edu/eam/ingesoft/bi/persistencia/enumeraciones/ClasesAuditorias.java
@@ -0,0 +1,6 @@
+package co.edu.eam.ingesoft.bi.persistencia.enumeraciones;
+
+public enum ClasesAuditorias {
+
+	AuditoriaPersona,AuditoriaDetalleVenta,AuditoriaArea,AuditoriaConexciones,AuditoriaUsuarios,AuditoriaTiposUsuarios,AuditoriaFacturaVenta,AuditoriaIventario,AuditoriaPermisos,AuditoriaProducto
+}
