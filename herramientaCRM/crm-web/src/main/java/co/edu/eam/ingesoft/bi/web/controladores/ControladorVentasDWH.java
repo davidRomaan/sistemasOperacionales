83536d86ce5b257f8e9382fd339aa9e8682e4a41
didierNar <yovanni.n.r@gmail.com>
2018-05-30 12:37:58 -0500

    commit

--- herramientaCRM/crm-web/src/main/java/co/edu/eam/ingesoft/bi/web/controladores/ControladorVentasDWH.java
+++ herramientaCRM/crm-web/src/main/java/co/edu/eam/ingesoft/bi/web/controladores/ControladorVentasDWH.java
@@ -8,6 +8,11 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 import co.edu.eam.ingesoft.bi.negocio.dwh.HechoVentasEJB;
 import co.edu.eam.ingesoft.bi.presistencia.entidades.datawh.HechoVentas;
 
@@ -16,15 +21,57 @@ import co.edu.eam.ingesoft.bi.presistencia.entidades.datawh.HechoVentas;
 public class ControladorVentasDWH implements Serializable {
 
 	private List<HechoVentas> hechosVenta;
-	
+
 	@EJB
 	private HechoVentasEJB hechoVentaEJB;
-	
+
 	@PostConstruct
-	private void cargarDatos(){		
-		hechosVenta = hechoVentaEJB.obtenerListaHechos(); 		
+	private void cargarDatos() {
+		hechosVenta = hechoVentaEJB.obtenerListaHechos();
 	}
-	
+
+	public void postProcessor(Object document) {
+
+		HSSFWorkbook wb = (HSSFWorkbook) document;
+		HSSFSheet sheet = wb.getSheetAt(0);
+		HSSFRow header;
+
+		int index = 0;
+
+		for (int j = 1; j < hechosVenta.size()+1; j++) {
+			header = sheet.getRow(j);
+
+			for (int i = 0; i < header.getPhysicalNumberOfCells(); i++) {
+				
+				HSSFCell cell = header.getCell(i);
+
+				if (i == 2) {
+					cell.setCellValue(hechosVenta.get(index).getProducto().getPrecio());
+				}
+				
+				if (i == 4){
+					cell.setCellValue(hechosVenta.get(index).getUnidades());
+				}
+				
+				if (i == 5){
+					cell.setCellValue(hechosVenta.get(index).getSubtotal());
+				}
+				
+				if (i == 8){
+					cell.setCellValue(hechosVenta.get(index).getPersona().getEdad());
+				}
+				
+				if (i == 11){
+					cell.setCellValue(hechosVenta.get(index).getEmpleado().getEdad());
+				}
+
+			}
+			
+			index++;
+
+		}
+
+	}
 
 	public List<HechoVentas> getHechosVenta() {
 		return hechosVenta;
@@ -33,8 +80,5 @@ public class ControladorVentasDWH implements Serializable {
 	public void setHechosVenta(List<HechoVentas> hechosVenta) {
 		this.hechosVenta = hechosVenta;
 	}
-	
-	
-	
-	
+
 }
